f55b5267b81f3fd2c4efe61cdf4af2f5245707ad
Matthew K <kehlmanm@gmail.com>
2013-08-23 19:43:45 -0400

    Requirejs shim for socketio

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,8 @@ require.config({
         jquery: '../bower_components/jquery/jquery',
         Backbone: '../bower_components/backbone-amd/backbone-min',
         underscore: '../bower_components/underscore/underscore-min',
-        bootstrap: 'vendor/bootstrap'
+        bootstrap: 'vendor/bootstrap',
+        socketio: '/socket.io/socket.io.js'
     },
     shim: {
         bootstrap: {
@@ -13,14 +14,21 @@ require.config({
         underscore: {
             deps: ['jquery'],
             exports: '_'
+        },
+        socketio: {
+            exports: 'io'
         }
     }
 });
 
-require(['app', 'jquery', 'underscore', 'Backbone', 'bootstrap'], function (app, $, _,Backbone) {
+require(['app', 'jquery', 'underscore', 'Backbone', 'socketio', 'bootstrap'], function (app, $, _, Backbone, io) {
     'use strict';
     // use app here
     console.log(app);
     console.log(Backbone);
+    var socket = io.connect('http://localhost:8080/');
+    socket.on('broadcasting', function (data) {
+        console.log(data);
+    });
     console.log('Running jQuery %s', $().jquery);
 });
